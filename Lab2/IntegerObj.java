f1564c0d51dcdc7ca2ac75bcd4ea2e9835f8d421
Adamcoakley <Adamcoakley18@gmail.com>
2021-10-13 16:27:38 +0100

    Lab2 files added.

---
+++ Lab2/IntegerObj.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab2;
+
+/**
+ * The Main program implements an application that
+ * displays the effect of
+ *
+ * @author  Adam Coakley
+ * @since   2021-10-11
+ *
+ */
+
+class IntegerObj {
+    int value;
+    IntegerObj(int val) {
+        this.value = val;
+    }
+    int inc(){
+        this.value++;
+        return this.value;
+    }
+}
