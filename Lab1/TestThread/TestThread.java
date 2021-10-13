d3116dff35229e164dece3c3d185b403508956ff
Adamcoakley <Adamcoakley18@gmail.com>
2021-10-13 16:18:29 +0100

    Working version of lab one.

---
+++ Lab1/TestThread/TestThread.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab1.TestThread;
+
+/**
+ *
+ * @author Adam Coakley
+ * @since   2021-10-11
+ *
+ * Create two threads and run them.
+ *
+ */
+
+public class TestThread {
+    public static void main(String args[]) {
+      ThreadDemo T1 = new ThreadDemo( "Thread-1");
+      T1.start();
+      ThreadDemo T2 = new ThreadDemo( "Thread-2");
+      T2.start();
+   } 
+}
