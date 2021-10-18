a78a46235f77dee97aee01a876785b308cc126e7
Adamcoakley <Adamcoakley18@gmail.com>
2021-10-18 09:12:34 +0100

    Lab2 synchronised exercise complete

---
+++ Lab2/synchronised/IntegerObj.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab2.synchronised;
+
+/**
+ *
+ * @author  Adam Coakley
+ * @since   2021-10-11
+ *
+ */
+
+class IntegerObj {
+    int value;
+    IntegerObj(int val) {
+        this.value = val;
+    }
+    synchronized int inc(){
+        this.value++;
+        return this.value;
+    }
+}
