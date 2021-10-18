34819f521e6ab2d63a1a9c363dae1bbd588cdf8d
Adamcoakley <Adamcoakley18@gmail.com>
2021-10-18 09:17:18 +0100

    Lab2 mutex exercise complete

---
+++ Lab2/mutex/IntegerObj.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab2.mutex;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * The Main program implements an application that
+ * displays the effect of
+ *
+ * @author  Adam Coakley
+ * @since   2021-10-11
+ *
+ */
+
+class IntegerObj {
+
+    private ReentrantLock mutex = new ReentrantLock();
+
+    int value;
+    IntegerObj(int val) {
+        this.value = val;
+    }
+    int inc(){
+        try {
+            mutex.lock();
+            this.value++;
+            return this.value;
+        } finally {
+            mutex.unlock();
+        }
+    }
+}
