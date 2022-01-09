a110e2959612b80584153b133b2de4afc7271d39
Adamcoakley <Adamcoakley18@gmail.com>
2022-01-09 00:17:25 +0000

    Updated Lab6

--- Lab6/ProducerConsumer.java
+++ Lab6/ProducerConsumer.java
@@ -2,79 +2,65 @@ package Lab6;
 
 import java.util.concurrent.Semaphore;
 
-public class ProducerConsumer {
-
+public class ProducerConsumer{
     public static void main(String[] args) {
 
-        Semaphore empty = new Semaphore(1);
-        Semaphore full = new Semaphore( 0);
-        Semaphore mutex = new Semaphore(1);
+        Semaphore producerSem = new Semaphore(1);
+        Semaphore consumerSem = new Semaphore(0);
 
-        Producer producer = new Producer(empty, full, mutex);
-        Consumer consumer = new Consumer(empty, full, mutex);
+        Producer producer = new Producer(producerSem, consumerSem);
+        Consumer consumer = new Consumer(consumerSem, producerSem);
 
-        Thread producerThread = new Thread(producer);
-        Thread consumerThread = new Thread(consumer);
+        Thread producerThread = new Thread(producer, "ProducerThread");
+        Thread consumerThread = new Thread(consumer, "ConsumerThread");
 
         producerThread.start();
         consumerThread.start();
     }
+}
 
-    static class Producer implements Runnable {
+class Producer implements Runnable{
 
-        Semaphore empty, full, mutex;
+    Semaphore producerSem, consumerSem;
 
-        public Producer(Semaphore empty, Semaphore full, Semaphore mutex){
-            this.empty = empty;
-            this.full = full;
-            this.mutex = mutex;
-        }
-
-        @Override
-        public void run() {
-            int index = 0;
-            while(index < 5){
-                try{
-                    empty.acquire();
-                    mutex.acquire();
-                    Thread.sleep(200);
-                    System.out.println("Producer produced item.");
-                    mutex.release();
-                    full.release();
-                    index--;
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+    public Producer(Semaphore producerSem,Semaphore consumerSem) {
+        this.producerSem = producerSem;
+        this.consumerSem = consumerSem;
     }
 
-    static class Consumer implements Runnable {
+    public void run() {
+        for(int index=0; index<5; index++){
+            try {
+                producerSem.acquire();
+                System.out.println("Producer produced item: " +index);
+                consumerSem.release();
 
-        Semaphore empty, full, mutex;
-
-        public Consumer(Semaphore empty, Semaphore full, Semaphore mutex){
-            this.empty = empty;
-            this.full = full;
-            this.mutex = mutex;
-        }
-
-        @Override
-        public void run() {
-            int index = 5;
-            while(index > 0){
-                try{
-                    full.acquire();
-                    mutex.acquire();
-                    Thread.sleep(200);
-                    System.out.println("Consumer consumed item.");
-                    mutex.release();
-                    empty.release();
-                    index--;
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
 }
+
+class Consumer implements Runnable{
+
+    Semaphore producerSem, consumerSem;
+
+    public Consumer(Semaphore consumerSem,Semaphore producerSem) {
+        this.consumerSem=consumerSem;
+        this.producerSem=producerSem;
+    }
+
+    public void run() {
+        for(int index=0; index<5; index++){
+            try {
+                consumerSem.acquire();
+                System.out.println("Consumer consumed item: "+index);
+                producerSem.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
