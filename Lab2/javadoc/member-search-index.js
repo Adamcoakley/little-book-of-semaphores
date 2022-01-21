57f9a038ad79cc357b8a851710c0bb5425cd6600
Adamcoakley <Adamcoakley18@gmail.com>
2022-01-21 16:30:10 +0000

    Updated javadoc

---
+++ Lab2/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"Lab2","c":"Task","l":"name"},{"p":"Lab2","c":"Task","l":"run()"},{"p":"Lab2","c":"Task","l":"Task(String, IntegerObj)","u":"%3Cinit%3E(java.lang.String,Lab2.IntegerObj)"},{"p":"Lab2","c":"Task","l":"total"}];updateSearchResults();
